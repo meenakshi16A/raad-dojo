ffc39887337def1a8b1dae7f136e6fdcdf5bec00
narendra <m.suhel11@gmail.com>
2022-03-07 15:30:50 +0530

    make it better

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
-  const { email, name, password } = req.body;
+  const {email} = req.body;
 
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
@@ -13,9 +13,7 @@ const register = async (req, res) => {
 
   // first registered user is an admin
   const isFirstAccount = (await User.countDocuments({})) === 0;
-  const role = isFirstAccount ? 'admin' : 'user';
-
-  const user = await User.create({ name, email, password, role });
+  const user = await User.create(req.body);
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
