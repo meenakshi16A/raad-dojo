5e7f60686f4b495bde119fa3341ad00b202ff961
narendra <m.suhel11@gmail.com>
2022-03-07 15:49:16 +0530

    make it better

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,7 +35,7 @@ const login = async (req, res) => {
   }
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
-
+console.log(tokenUser)
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
