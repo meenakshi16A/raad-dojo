07caa909f0eb5ccc31eacd9ab8c6f9022d9e5da2
narendra <m.suhel11@gmail.com>
2022-03-07 16:11:46 +0530

    make it better

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.use(
     max: 60,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(helmet());
 app.use(cors());
 app.use(xss());
