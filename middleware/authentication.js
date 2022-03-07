14f682e127d6e59b03cb5da4cfc4b9f747926205
narendra <m.suhel11@gmail.com>
2022-03-07 17:08:59 +0530

    auth set

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -19,11 +19,7 @@ const authenticateUser = async (req, res, next) => {
 
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      throw new CustomError.UnauthorizedError(
-        'Unauthorized to access this route'
-      );
-    }
+
     next();
   };
 };
