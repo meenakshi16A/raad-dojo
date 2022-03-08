086dbe2fda618da1a9c3096ce62a0d5c29a4af9f
narendra <m.suhel11@gmail.com>
2022-03-08 11:31:02 +0530

    auth set

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,7 +102,7 @@ const otpverify = async (req,res) => {
 }
 
 const setpassword =async (res,req)=>{
-  console.log(req.body.email)
+  console.log(req.body)
   const uinfo = await User.findOneAndUpdate({email:`meenakshiibr@gmail.com`},{password:req.body.password})
   if(!uinfo)
   {
